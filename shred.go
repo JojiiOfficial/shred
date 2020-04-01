1a2a19c3dccb4ac5bf59738041aa763df642421c
JojiiOfficial <jojii@jojii.de>
2020-04-01 18:25:01 +0200

    improve shredding

--- shred.go
+++ shred.go
@@ -8,30 +8,54 @@ import (
 	"sync"
 )
 
-var wg sync.WaitGroup
+// Shredder a shredder
+type Shredder struct {
+	wg sync.WaitGroup
+}
 
-// Conf is a object containing all choices of the user
-type Conf struct {
-	Times  int
-	Zeros  bool
-	Remove bool
+// ShredderConf is a object containing all choices of the user
+type ShredderConf struct {
+	Shredder     *Shredder
+	WriteOptions WriteOptions
+	Times        int
+	Delete       bool
+}
+
+// WriteOptions options how to shred
+type WriteOptions uint8
+
+// Available write options
+const (
+	WriteZeros WriteOptions = iota
+	WriteRand
+	WriteRandSecure
+)
+
+// NewShredderConf create a new shredder
+func NewShredderConf(shredder *Shredder, options WriteOptions, times int, delete bool) *ShredderConf {
+	return &ShredderConf{
+		Shredder:     shredder,
+		WriteOptions: options,
+		Times:        times,
+		Delete:       delete,
+	}
 }
 
 // Path shreds all files in the location of path
 // recursively. If remove is set to true files will be deleted
 // after shredding. When a file is shredded its content
 // is NOT recoverable so !!USE WITH CAUTION!!
-func (conf Conf) Path(path string) error {
+func (shredderConf *ShredderConf) Path(path string) error {
 	stats, err := os.Stat(path)
 	if err != nil {
 		return err
 	} else if stats.IsDir() {
-		err := conf.Dir(path)
+		err := shredderConf.Dir(path)
 		if err != nil {
 			return err
 		}
 	} else {
-		err := conf.File(path)
+		err := shredderConf.ShredFile(path)
 		if err != nil {
 			return err
 		}
@@ -40,92 +64,115 @@ func (conf Conf) Path(path string) error {
 }
 
 // Dir overwrites every File in the location of path and everything in its subdirectories
-func (conf Conf) Dir(path string) error {
+func (shredderConf *ShredderConf) Dir(path string) error {
+	// For each file
 	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+
 		stats, _ := os.Stat(path)
 
 		if !stats.IsDir() {
-			wg.Add(1)
-			go conf.File(path)
-			wg.Wait()
+			shredderConf.Shredder.wg.Add(1)
+			go shredderConf.ShredFile(path)
+			shredderConf.Shredder.wg.Wait()
 		}
 		return nil
 	})
 	return err
 }
 
-// File overwrites a given File in the location of path
-func (conf Conf) File(path string) error {
+// ShredFile overwrites a given ShredFile in the location of path
+func (shredderConf *ShredderConf) ShredFile(path string) error {
+	// Get fileinfo
 	fileinfo, err := os.Stat(path)
 	if err != nil {
 		return err
 	}
 	size := fileinfo.Size()
-	err = conf.WriteRandom(path, size)
-	if err != nil {
-		return err
+
+	// Write secure rand
+	if shredderConf.WriteOptions&WriteRandSecure == WriteRandSecure {
+		err = shredderConf.WriteRandomSecure(path, size)
+		if err != nil {
+			return err
+		}
 	}
-	err = conf.WriteZeros(path, size)
-	if err != nil {
-		return err
+
+	// Write zeros if desired
+	if shredderConf.WriteOptions&WriteZeros == WriteZeros {
+		err = doWriteZeros(path, size)
+		if err != nil {
+			return err
+		}
 	}
-	if conf.Remove {
+
+	// Delete file if desired
+	if shredderConf.Delete {
 		err := os.Remove(path)
 		if err != nil {
 			return err
 		}
 	}
-	wg.Done()
+
+	shredderConf.Shredder.wg.Done()
 	return nil
 }
 
-// WriteRandom overwrites a File with random stuff.
-// conf.Times specifies how many times the file should be overwritten
-func (conf Conf) WriteRandom(path string, size int64) error {
-	for i := 0; i < conf.Times; i++ {
-		file, err := os.OpenFile(path, os.O_RDWR, 0)
+// WriteRandomSecure overwrites a File with random stuff.
+func (shredderConf *ShredderConf) WriteRandomSecure(file string, size int64) error {
+	// Do n times. Specified in conf
+	for i := 0; i < shredderConf.Times; i++ {
+		// Open file
+		f, err := os.OpenFile(file, os.O_RDWR, 0)
 		if err != nil {
 			return err
 		}
-		defer file.Close()
-		offset, err := file.Seek(0, 0)
+
+		// Seek to start
+		offset, err := f.Seek(0, 0)
 		if err != nil {
 			return err
 		}
+
+		// Read random
 		buff := make([]byte, size)
 		_, err = rand.Read(buff)
 		if err != nil {
 			return err
 		}
-		_, err = file.WriteAt(buff, offset)
+
+		// Write random
+		_, err = f.WriteAt(buff, offset)
 		if err != nil {
 			return err
 		}
-		file.Close()
+
+		// Close file
+		f.Close()
 	}
+
 	return nil
 }
 
-// WriteZeros overwrites a File with zeros if conf.Zeros == true
-func (conf Conf) WriteZeros(path string, size int64) error {
-	if !conf.Zeros {
-		return nil
-	}
-
+func doWriteZeros(path string, size int64) error {
+	// Open file
 	file, err := os.OpenFile(path, os.O_RDWR, 0)
 	if err != nil {
 		return err
 	}
 
-	defer file.Close()
+	// Goto 0
 	offset, err := file.Seek(0, 0)
 	if err != nil {
 		return err
 	}
+
+	// Create zeroed buffer and write it
 	buff := make([]byte, size)
 	_, err = file.WriteAt(buff, offset)
+
+	file.Close()
 	return err
 }
